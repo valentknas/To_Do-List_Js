9518d026d7b31cebe69775306c42d61f0ce6801e
Valentina <valentnaran@gmail.com>
2025-09-17 18:15:18 -0500

    agregue la funcionalidad de poder

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 
 
 const input = document.getElementById('to-do-input');
-const addBtn = document.getElementById('item-to-do');
+const addBtn = document.getElementById('add-btn');
 const ToDoList = document.getElementById('cont-to-do-list');
-const completeList = document.getElementById('complete-list');
+const completeList = document.getElementById('cont-complete-list');
 
 // creamos la funcion que nos permite crear una nueva tarea a partir del formulario
 
@@ -24,7 +24,7 @@ function createToDoItem (textoItem) {
 
     // creamos el ultimo nodo hijo, el bottom de eliminar
 
-    const deleteBtn = document.createElement('buttom');
+    const deleteBtn = document.createElement('button');
     deleteBtn.textContent ='X';
 
     // enzamblamos dentro del nodo padre sus nodos hijos, es decir la estructura de la tarea
@@ -34,9 +34,22 @@ function createToDoItem (textoItem) {
     item.appendChild(deleteBtn);
     // utilizamos el return para retornar o para dar respuesta al elemento creado ya que lo utilizaremos
     return item;
-    
-
 
 
 };
 
+// Detectamos el evento click sobre el bottom agregar con un evento de escucha o de listen
+// para que a partir de este evento se agregue la tarea dentro del contenedor cont-to-do-list
+
+addBtn.addEventListener('click',()=>{
+    const textoItem = input.value.trim();
+    if (textoItem =="") {
+        alert('No se puede crear una tarea vacia');
+    }
+
+    else{
+        const newItem = createToDoItem (textoItem);
+        ToDoList.appendChild(newItem);
+        input.value ="";
+    }
+});
